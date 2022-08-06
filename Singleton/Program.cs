9951a9265e950c606a58cbb4459a1db880dda681
benjamin_zhou <calmzhou@gmail.com>
2022-08-05 23:38:55 -0500

    create repo for DesignPatterns

---
+++ Singleton/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var singleton1 = Singleton.Instance;
+            var singleton2 = Singleton.Instance;
+            Console.WriteLine(singleton1.GetHashCode()==singleton2.GetHashCode());
+
+            //for (int i = 0; i < 20; i++)
+            //{
+            //    var singleton1 = Singleton.Instance;
+            //    var singleton1 = Singleton.Instance;
+
+            //}
+        }
+    }
+
+}
